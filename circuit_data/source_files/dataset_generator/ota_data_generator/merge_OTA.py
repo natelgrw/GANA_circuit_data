96ddbcb97ac573a3c5398bd36453003abf77fb62
natelgrw <nate.lgrw@gmail.com>
2025-01-27 18:28:29 -0500

    Fixed merge_OTA operations + Created FULL_OTA merged netlists

--- circuit_data/source_files/dataset_generator/ota_data_generator/merge_OTA.py
+++ circuit_data/source_files/dataset_generator/ota_data_generator/merge_OTA.py
@@ -1,10 +1,15 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 """
 Created on Tue Oct 22 21:06:36 2019
 
 @author: kunal001
+
+Revised on Mon Jan 27 18:38:14 2025
+
+@editor: natelgrw
 """
+
+# !/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
 import os
 import logging
@@ -14,139 +19,145 @@ if not os.path.exists("./LOG"):
 elif os.path.exists("./LOG/read_netlist.log"):
     os.rename("./LOG/read_netlist.log", "./LOG/read_netlist.log1")
     
-logging.basicConfig(filename='./LOG/read_netlist.log', level=logging.DEBUG)
+logging.basicConfig(filename = "./LOG/read_netlist.log", level = logging.DEBUG)
 
-OTA_DIR = "OTA"
-BIAS_DIR = "BIAS"
-LG_DIR = "Local_generation"
+# Establishing lists of netlist components
+OTA_DIR = "ota_unbiased"
+BIAS_DIR = "bias"
+LG_DIR = "local_generation"
 
 OTA_NETLIST = os.listdir(OTA_DIR)
 LG_NETLIST = os.listdir(LG_DIR)
 BIAS_NETLIST = os.listdir(BIAS_DIR)
 
-
+# Class containing functions joining circuit pins and logging pin errors
 class create_OTA_connection:
-    
-    def __init__(self,line):
-        self.line =line
-        self.bias_ports =[]
-        self.LG_connect_pins =[]
+    def __init__(self, line):
+        self.line = line
+        self.bias_ports = []
+        self.LG_connect_pins = []
         self.insts = []
         self.LG_input = None
         self.ota_inst()
+
     def ota_inst(self):
-        words=line.split()
+        words = line.split()
         for idx, port in enumerate(words):
             if 'bias' in port:
-                words[idx]="LG_"+port
-                self.bias_ports.append("LG_"+port)
-        self.insts.append(  "\nxiota "+ " ".join(words[2:]) + \
-                                " "+ words[1])
-    def LG_pins(self,lg_pins,lg_type):
+                words[idx] = "LG_" + port
+                self.bias_ports.append("LG_" + port)
+        self.insts.append("\nxiota " + " ".join(words[2:]) + " " + words[1])
         
-        if "LG_Vbiasn2" in  self.bias_ports:
+    def LG_pins(self, lg_pins, lg_type):
+        if "LG_Vbiasn2" in self.bias_ports:
             if "Vbiasn2" not in lg_pins:
-                logging.error("Vbiasn2 not in lg_pins:%s,%s",self.insts[0],lg_type)
+                logging.error("Vbiasn2 not in lg_pins:%s,%s", self.insts[0], lg_type)
                 return 0
-        elif "LG_Vbiasp2" in  self.bias_ports:
+        elif "LG_Vbiasp2" in self.bias_ports:
             if "Vbiasp2" not in lg_pins:
-                logging.error("Vbiasp2 not in lg_pins:%s,%s",self.insts[0],lg_type)
+                logging.error("Vbiasp2 not in lg_pins:%s,%s", self.insts[0], lg_type)
                 return 0
-        elif "LG_Vbiasn2" not in  self.bias_ports:
+        elif "LG_Vbiasn2" not in self.bias_ports:
             if "Vbiasn2" in lg_pins:
-                logging.error("Vbiasn2 in lg_pins:%s,%s",self.insts[0],lg_type)
+                logging.error("Vbiasn2 in lg_pins:%s,%s", self.insts[0], lg_type)
                 return 0
-        elif "LG_Vbiasp2" not in  self.bias_ports:
+        elif "LG_Vbiasp2" not in self.bias_ports:
             if "Vbiasp2" in lg_pins:
-                logging.error("Vbiasp2 in lg_pins:%s,%s",self.insts[0],lg_type)
+                logging.error("Vbiasp2 in lg_pins:%s,%s", self.insts[0], lg_type)
                 return 0
-        if "LG_Vbiasn1" not in  self.bias_ports:
+        elif "LG_Vbiasn1" not in self.bias_ports:
             if "Vbiasn1" in lg_pins:
-                logging.error("Vbiasn1 in lg_pins:%s,%s",self.insts[0],lg_type)
+                logging.error("Vbiasn1 in lg_pins:%s,%s", self.insts[0], lg_type)
                 return 0 
-        elif "LG_Vbiasp1" not in  self.bias_ports:
+        elif "LG_Vbiasp1" not in self.bias_ports:
             if "Vbiasp1" in lg_pins:
-                logging.error("Vbiasp1 in lg_pins:%s,%s",self.insts[0],lg_type)
+                logging.error("Vbiasp1 in lg_pins:%s,%s", self.insts[0], lg_type)
                 return 0 
-        words=lg_pins.split()[1:]
-        added = 0         
+            
+        words = lg_pins.split()[1:]
+        added = 0
+
         for idx, port in enumerate(words):
             if 'bias' in port and port.endswith('O'):
-                check_port = "LG_"+port.replace(':O','')
+                check_port = "LG_"+port.replace(':O', '')
                 words[idx] = check_port
                 if check_port in self.bias_ports:
                     self.LG_connect_pins.append(check_port)
                     added = 1
             elif port.endswith(':I') and 'ias' in port:
-                self.LG_input = port.replace(':I','') 
+                self.LG_input = port.replace(':I', '') 
                 words[idx]= self.LG_input
             else:
-                words[idx] = port.replace(':I','').replace(':O','')
+                words[idx] = port.replace(':I', '').replace(':O', '')
            
-        if added ==1: 
-            self.insts.append("\nxi"+lg_type+" " +" ".join(words) +" "+lg_type)
+        if added == 1: 
+            self.insts.append("\nxi" + lg_type + " " + " ".join(words) + " " + lg_type)
             return 1
         else:
             return 0
         
-    def BIAS_pins(self,bias_line):
-        words=bias_line.split()
+    def BIAS_pins(self, bias_line):
+        words = bias_line.split()
         for idx, port in enumerate(words):
-            port = port.replace("Vbias","Bias")
+            port = port.replace("Vbias", "Bias")
             words[idx] = port
-            if self.LG_input ==port:
-                self.insts.append("\nxib"+words[1]+" "+ \
-                                  " ".join(words[2:])+ " "+words[1])
+            if self.LG_input == port:
+                self.insts.append("\nxib" + words[1] + " " + " ".join(words[2:]) + " " + words[1])
                 return 1
 
         return 0
 
+# Parses files in the local_generation folder to find pin connections
 def read_LG_bias(file, connect):
-    BIAS_lines=[]
-    found=0
+    BIAS_lines = []
+    found = 0
     if 'LG' in file:
-        FLAG =0
+        FLAG = 0
         with open(file, "r") as file:
-            name =""
+            name = ""
             for line in file:
                 if ".SUBCKT" in line: 
                     name = line.split()[1]
-                    FLAG=1
+                    FLAG = 1
                 elif ".PININFO" in line:
-                    found = connect.LG_pins(line,name)
+                    found = connect.LG_pins(line, name)
                 elif ".ENDS" in line:
-                    FLAG=0
+                    FLAG = 0
                     BIAS_lines.append(line)
-                if FLAG==1:
+                if FLAG == 1:
                     BIAS_lines.append(line)
+
     if found:
         return BIAS_lines
     else:
         return None          
-    
+
+# Parses files in the bias folder to find pin connections
 def read_bias(file, connect):
-    BIAS_lines=[]
-    found=0
+    BIAS_lines = []
+    found = 0
     if file.endswith('.sp'):
-        FLAG =0
+        FLAG = 0
         with open(file, "r") as file:
             for line in file:
                 if ".SUBCKT" in line: 
-                    found=connect.BIAS_pins(line)
-                    FLAG=1
+                    found = connect.BIAS_pins(line)
+                    FLAG = 1
                 elif ".ENDS" in line:
-                    FLAG=0
+                    FLAG = 0
                     BIAS_lines.append(line)
-                if FLAG==1:
+                if FLAG == 1:
                     BIAS_lines.append(line)
+
     if found:
         return BIAS_lines
     else:
         return None   
-              
+
+# Loops through combinations of OTA, LG, and bias netlists, writing conbined netlists to a new FULL_OTA directory 
 for ON in OTA_NETLIST:
     if "_" in ON:
-        with open(OTA_DIR+"/"+ON, "r") as file:
+        with open(OTA_DIR + "/" + ON, "r") as file:
             OTA_lines = []
             for line in file:
                 if ".SUBCKT" in line: 
@@ -155,19 +166,22 @@ for ON in OTA_NETLIST:
 
         for LN in LG_NETLIST:
             for BN in BIAS_NETLIST:
-                LG_lines =[]
-                BG_lines =[]
-                connect.insts=[connect.insts[0]]
-                check_lg = read_LG_bias(LG_DIR+"/"+LN, connect)
+                LG_lines = []
+                BG_lines = []
+                connect.insts = [connect.insts[0]]
+                check_lg = read_LG_bias(LG_DIR + "/" + LN, connect)
+
                 if check_lg:
-                    check_bias = read_bias(BIAS_DIR+"/"+BN, connect)
+                    check_bias = read_bias(BIAS_DIR + "/" + BN, connect)
                     if check_bias:
-                        print("Creating combination of: " +ON + ", " +LN + ", "+ BN)
-                        fo= open("FULL_OTA/"+ON+"_"+LN+"_"+BN,"w")
-                        LG_lines = LG_lines+check_lg
+                        print("Creating combination of: " + ON + ", " + LN + ", " + BN)
+
+                        fo = open("FULL_OTA/" + ON + "_" + LN + "_" + BN, "w")
+                        LG_lines = LG_lines + check_lg
                         BG_lines = BG_lines + check_bias
+
                         for line in OTA_lines:
-                            fo.write(line)
+                            fo.write(line)                           
                         fo.write("\n")
                         for line in LG_lines:
                             fo.write(line)
@@ -175,12 +189,8 @@ for ON in OTA_NETLIST:
                         for line in BG_lines:
                             fo.write(line)
                         fo.write("\n")
-                            
                         for connect_line in connect.insts:
                             fo.write(connect_line)
-                
+
                         fo.write("\n.END")
                         fo.close
-            
-        
-        
